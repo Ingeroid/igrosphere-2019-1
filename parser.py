115d82777cb024cf016ac29630a3bcc6c66f615e
Ingeroid <48728914+Ingeroid@users.noreply.github.com>
2019-03-23 00:24:25 +0300

    Second repair script
    Now script show ONLY that lines that match regexp

--- parser.py
+++ parser.py
@@ -1,10 +1,8 @@
 import os, re
 
-
 def read_file(file_path):
     f = open(file_path + filename, "r")
     return f.readlines()
-
 
 def filter_lines(regexp, lines):
     result = []
@@ -12,17 +10,13 @@ def filter_lines(regexp, lines):
         while line != " ":
             if re.match(regexp, line):
                 result.append(line)
-            else:
-                print(f"line {line} don't match")
             break
     return [line for line in lines if re.match(regexp, line)]
 
-
+filename = "\\coverage-error.log"
+file_path = os.getcwd()
+regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
 if __name__ == "__main__":
-    filename = "\\coverage-error.log"
-    file_path = os.getcwd()
-    print(file_path + filename)
-    regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
     lines = read_file(file_path)
     print("Author is s.rodionov")
     print(filter_lines(regexp, lines))
