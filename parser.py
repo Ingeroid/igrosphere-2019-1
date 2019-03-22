2c290880c411b44ad5e960c6013e3336b52e4f63
Ingeroid <48728914+Ingeroid@users.noreply.github.com>
2019-03-23 00:18:55 +0300

    Repair script
    Now script show lines that match regexp

--- parser.py
+++ parser.py
@@ -1,23 +1,30 @@
-import re
+import os, re
 
 
 def read_file(file_path):
-    with open(file_path, 'r')as f:
-        return f.read()
+    f = open(file_path + filename, "r")
+    return f.readlines()
 
 
 def filter_lines(regexp, lines):
     result = []
     for line in lines:
-        if re.match(regexp, line):
-            result.append(line)
-        else:
-            print(f"line {line} don't match")
+        while line != " ":
+            if re.match(regexp, line):
+                result.append(line)
+            else:
+                print(f"line {line} don't match")
+            break
     return [line for line in lines if re.match(regexp, line)]
 
+
 if __name__ == "__main__":
-    filename = "coverage-error.log"
-    regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:d\d\].+"
-    lines = read_file(filename)
-    print("Author is d.galamaga")
+    filename = "\\coverage-error.log"
+    file_path = os.getcwd()
+    print(file_path + filename)
+    regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
+    lines = read_file(file_path)
+    print("Author is s.rodionov")
+    print(filter_lines(regexp, lines))
+    lines = read_file(file_path)
     print(filter_lines(regexp, lines))
